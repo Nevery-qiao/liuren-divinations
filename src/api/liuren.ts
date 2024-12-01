c235232f32bf3bf7a9b76837bcc8e15c8bd7e67d
🎹乔 <qiaoyang@qiaodeMacBook-Air.local>
2024-12-02 02:14:29 +0800

    update API URL to use full path

--- src/api/liuren.ts
+++ src/api/liuren.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Lunar } from 'lunar-typescript';
 
-const apiUrl = '/api/2024/xlr/pan.php';
+const apiUrl = 'http://demo1.w258.cn/2024/xlr/pan.php';
 
 export interface GongInfo {
     position: string;    // 宫位名称
